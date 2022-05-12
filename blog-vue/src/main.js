71e2264962d35db6e5a35f629bfa9b3592a00728
ershuaili <liershuai.cn@gmail.com>
2022-05-12 21:33:22 +0800

    feat: 前端优化

--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8899'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
